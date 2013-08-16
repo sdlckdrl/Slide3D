2808a9134ba27c2e7f1db1849c97e5ba0836e060
sdlckdrl <sdlckdrl@gmail.com>
2013-08-17 00:10:24 +0900

    이미지 변경

--- my.js
+++ my.js
@@ -1,24 +1,15 @@
 $(function(){
 
-	$(".slide1").slide3D({
+	jQuery(".slide").slide3D({
 	    speed: 1000,
-	    btnGo: $("#aaa .btngo"),
-	    btnNext : $("#aaa .btnnext"),
-	    btnPrev : $("#aaa .btnprev"),
-	    vertical : true,
-  	    //easing : "easeInOutElastic",
-  	    auto:3000
-	});
-
-	jQuery(".slide2").slide3D({
-	    speed: 1000,
-	    btnGo: $("#bbb .btngo"),
-	    btnNext : $("#bbb .btnnext"),
-	    btnPrev : $("#bbb .btnprev"),
+	    btnGo: $(".navi>.btn_area>li>a"),
+	    btnNext : $(".navi>.control .next"),
+	    btnPrev : $(".navi>.control .prev"),
+	    btnStop : $(".navi>.control .pause"),
 	    vertical : false,
 //  	easing : "easeInOutElastic",
-// 	    auto:3000
-		start : 7
+ 	    auto:5000,
+		start : 1
 	});
 
 });
